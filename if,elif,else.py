4aaac669379eb7481dff9b5191ad7129bb7f5755
DHARSHINI MAHENDRAN <dharshinikalaiselvi1979@gmail.com>
2026-01-06 16:22:25 +0530

    if
    I MISSED ONE OF THE FILE

---
+++ if,elif,else.py
@@ -0,0 +1,11 @@
+n=int(input())
+if(n>89):
+    print("Grade A")
+elif(n<90 and n>69):
+    print("Grade B")
+elif(n<70 and n>49):
+    print("Grade C")
+elif(n<50 and n>34):
+    print("Grade D")
+else:
+    ("Fail")
